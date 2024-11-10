fbfd3a7b9911c9665f56f370689cb6e9aabda2ca
unknown <montielfernandojavier@gmail.com>
2024-11-09 23:09:49 -0300

    armado de la estructura inicial del proyecto con cucumber. Pero aún sin reportes

--- automation_exercise/cypress/support/e2e.js
+++ automation_exercise/cypress/support/e2e.js
@@ -1,20 +1,12 @@
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import './custom_commands/cmd-login';
 
-// Import commands.js using ES2015 syntax:
-import './commands'
+on("task", {
+    log(message) {
+      console.log(message);
+      return null;
+    },
+  });
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+  on("uncaught:exception", (err, runnable) => {
+    return false;
+  });
